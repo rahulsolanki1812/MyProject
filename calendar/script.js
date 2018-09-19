1a6093316ad6b8565b21eff14b768562ac8c2bd7
Rahul Singh Solanki <rahul.solanki@newput.com>
2018-09-19 09:11:11 +0530

    Calendar update

--- calendar/script.js
+++ calendar/script.js
@@ -1,66 +1,155 @@
 var calendar = {
     weekdays : ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],
-    months : ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    months : ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    birthday : [{"month":"January","birthday":[{"date":31,"person":"Sagar Mishra"}]},{"month":"February","birthday":[]},{"month":"March","birthday":[]},{"month":"April","birthday":[]},{"month":"May","birthday":[]},{"month":"June","birthday":[{"date":6,"person":"ashwin"}]},{"month":"July","birthday":[]},{"month":"August","birthday":[{"date":21,"person":"Rahul"},{"date":21,"person":"Tarun"}]},{"month":"September","birthday":[{"date":15,"person":"shivam"}]},{"month":"October","birthday":[{"date":18,"person":"Piyush"}]},{"month":"November","birthday":[]},{"month":"December","birthday":[]}]
+};
 
+var _firstSelectedDate = null;
+
+function dateSelector(lastSelectedDate) {
+    if (_firstSelectedDate === null) {
+        _firstSelectedDate = lastSelectedDate;
+        var li = document.querySelector('li[onclick="dateSelector('+ lastSelectedDate +')"]');
+        li.style.backgroundColor = "#0011ff80";
+        li.style.color = "#ffffff";
+    }
+    else {
+        var myBtn = document.getElementById('myBtn');
+        var modalContent = [];
+
+        if (_firstSelectedDate < lastSelectedDate) {
+            for (var i = _firstSelectedDate; i <= lastSelectedDate; i++) {
+                var li = document.querySelector('li[onclick="dateSelector('+ i +')"]');
+                li.setAttribute('class','selected');
+                modalContent.push(li.firstChild.nodeValue);
+            }
+        } else {
+            for (var i = lastSelectedDate; i <= _firstSelectedDate; i++) {
+                var li = document.querySelector('li[onclick="dateSelector('+ i +')"]');
+                li.setAttribute('class','selected');
+                modalContent.push(li.firstChild.nodeValue);
+            }
+        }
+        myBtn.click();
+        _firstSelectedDate = null;
+        document.getElementById('selected-content').innerHTML = modalContent.join(", ");
+    }
 }
 
 
-function getPreviousMonthData() {
+function onChangeYear() {
     var month = document.getElementById('month-name').innerHTML;
-    var year = document.getElementById('year').innerHTML;
+    var year = document.getElementById('year').value;
+    createCalendar(new Date(year, (calendar.months.indexOf(month) + 1), 0 ));
+}
+
+
+function onModalClose() {
+    var message = document.querySelector('input[type="text"]').value;
+    var indexes = (document.getElementById('selected-content').innerHTML).split(', ');
+    var selectedDates = (document.querySelector('li[onclick="dateSelector('+ indexes[0] +')"]'));
+    selectedDates.lastChild.innerHTML = message;
+    for (var i = 0; i < indexes.length; i++) {
+        selectedDates = (document.querySelector('li[onclick="dateSelector('+ indexes[i] +')"]'));
+        selectedDates.removeAttribute('class');
+
+        if (message != "") {
+            selectedDates.lastChild.setAttribute('class','event-box');
+        }
+    }
+};
+
+//Generate previous month data (when clicked on previous button on jumbotron)
+function getPreviousMonth() {
+    var month = document.getElementById('month-name').innerHTML;
+    var year = document.getElementById('year').value;
     createCalendar(new Date(year, calendar.months.indexOf(month), 0 ));
 }
 
-function getNextMonthData() {
+//Generate next month data (when clicked on next button on jumbotron)
+function getNextMonth() {
     var month = document.getElementById('month-name').innerHTML;
-    var year = document.getElementById('year').innerHTML;
-    console.log(calendar.months.indexOf(month) + 1);
-    createCalendar(new Date(year, (calendar.months.indexOf(month)+2), 0 ));
+    var year = document.getElementById('year').value;
+    createCalendar(new Date(year, (calendar.months.indexOf(month) + 2), 0 ));
 }
 
-//Create calendar will create date and year passed in it.
+//get previous month disabled weekdays
+function previousMonthDates(date) {
+    var lastMonthDate = new Date(date.getFullYear(), date.getMonth(), 0);
+    return lastMonthDate.getDate() - lastMonthDate.getDay();
+}
+
+//Get last date of given date object
+function getLastDateOfMonth(currentMonth) {
+    return new Date(currentMonth.getFullYear(), currentMonth.getMonth() + 1, 0).getDate();
+}
+
 function createCalendar(subDate) {
-    var superDate = new Date();
+    var today = new Date();
+
     if (subDate === undefined) {
-        var subDate = new Date(superDate.getFullYear(), superDate.getMonth()+1, 0);
-    }
-    var days = document.querySelector('.days');
-    days.innerHTML = "";
-    let counter = 1;
-    for(let i = 0; i < 7; i++ ) {
-        var li = document.createElement('li');
-        li.append(document.createTextNode(calendar.weekdays[i]));
-        days.append(li);
+        subDate = new Date(today.getFullYear(), today.getMonth() + 1, 0);
     }
 
-    var noOfDays = subDate.getDate();
-    var ulDays = document.querySelector('.dates');
-    ulDays.innerHTML = "";
-    subDate = new Date(subDate.getFullYear(),subDate.getMonth(),1);
-    for(let i = 0; i < (subDate.getDay() + noOfDays); i++ ) {
-        let id = "id" + i;
+    var monthStartDate = new Date(subDate.setDate(1));
+    var lastMonthDates = previousMonthDates(monthStartDate);
+    var dateContainer = document.querySelector('.dates');
+    var endingDate = getLastDateOfMonth(subDate);
+    var currentMonthDates = (monthStartDate.getDay() + endingDate);
+    dateContainer.innerHTML = "";
 
-        var li = document.createElement('li');
-        li.setAttribute('id',id);
-        ulDays.append(li);
-        if( i < subDate.getDay()) {
-            document.getElementById(id).innerHTML = "";
-            document.getElementById(id).style.border = "0";
-            document.getElementById(id).style.backgroundColor = "#ffffff";
-        }
-        else {
-            document.getElementById(id).innerHTML = counter;
-            counter++;
+    // Print calender according to day on 1st of month
+    for(var i = 0; i < currentMonthDates; i++ ) {
+        var date = document.createElement('li');
+        var eventBox = document.createElement('div');
+
+        if( i < monthStartDate.getDay()) {
+            dateContainer.innerHTML += "<li class='disabled'>" + lastMonthDates + "</li>";
+            lastMonthDates++;
+        } else {
+            dateContainer.innerHTML += "<li onclick='dateSelector(" + monthStartDate.getDate() + ")'>" + monthStartDate.getDate() + "<div></div></li>";
+            if(i == currentMonthDates-1) {
+                var currentMonthDates = (6 - monthStartDate.getDay());
+                for( var j = 0; j < currentMonthDates; j++) {
+                    monthStartDate.setDate(monthStartDate.getDate() + 1);
+                    dateContainer.innerHTML += "<li class='disabled'>"+ monthStartDate.getDate() +"</li>";
+                }
+                monthStartDate.setMonth(monthStartDate.getMonth()-1);
+            }
+            monthStartDate.setDate(monthStartDate.getDate()+1);
         }
     }
 
     // Show Current Date In Calendar - Checks current year, month with current calendar month & year then changes it.
-    if( (superDate.getFullYear() == subDate.getFullYear()) && (superDate.getMonth() == subDate.getMonth()) ) {
-        let id = "id" + (superDate.getDate() - 1 + subDate.getDay());
-        (document.getElementById(id)).setAttribute('id','current');
+    if( (today.getFullYear() == monthStartDate.getFullYear()) && (today.getMonth() == monthStartDate.getMonth()) ) {
+        var todaysDate = document.querySelector('li[onclick="dateSelector(' + today.getDate() + ')"]');
+        todaysDate.setAttribute('class', 'current');
     }
 
-    document.getElementById('month-name').innerHTML = calendar.months[subDate.getMonth()];
-    document.getElementById('year').innerHTML = ""+subDate.getFullYear();
+    // By changing calendar month/year update jumbotron with changed month/year
+    document.getElementById('month-name').innerHTML = calendar.months[monthStartDate.getMonth()];
+    document.getElementById('month-name').setAttribute('onchange','getNextMonth()');
+
+    document.getElementById('year').value = monthStartDate.getFullYear();
+    document.getElementById('year').setAttribute('onchange','onChangeYear()');
 }
-createCalendar();
+
+function generateDaysAndYear() {
+
+    var days = document.querySelector('.days');
+    for(var i = 0; i < 7; i++ ) {
+        days.innerHTML += "<li>" + calendar.weekdays[i] + "</li>";
+    }
+
+    var years = document.getElementById('year');
+    for(var i = 1996; i < 2025; i++) {
+        years.innerHTML += "<option>" + i + "</option>";
+    }
+}
+
+var initilize = function () {
+    generateDaysAndYear();
+    createCalendar();
+};
+
+initilize();
